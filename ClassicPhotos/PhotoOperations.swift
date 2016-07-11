17800dea643331472c556c7209d973162b889ae4
Andrei Chernushou <chernyshev.andy@gmail.com>
2016-07-11 22:13:15 +0300

    NSOperations created for background proccese.

--- ClassicPhotos/PhotoOperations.swift
+++ ClassicPhotos/PhotoOperations.swift
@@ -82,6 +82,53 @@ class ImageDownloader: NSOperation {
     }
 }
 
+class ImageFiltration:NSOperation {
+    
+    let photoRecord:PhotoRecord
+    
+    init(photoRecord:PhotoRecord) {
+        self.photoRecord = photoRecord
+    }
+    
+    override func main() {
+        
+        if self.cancelled {
+            return
+        }
+        
+        if self.photoRecord.state != .Downloaded {
+            return
+        }
+        
+        if let filteredImage = self.applySepiaFilter(self.photoRecord.image!) {
+            
+        }
+        
+    }
+    
+    func applySepiaFilter(image:UIImage) -> UIImage? {
+        
+        let inputImage = CIImage(data: UIImagePNGRepresentation(image)!)
+        
+        if self.cancelled {
+            return nil
+        }
+        
+        let context = CIContext(options: nil)
+        let filter = CIFilter(name: "CISepiaTone")
+        filter?.setValue(inputImage, forKey: kCIInputImageKey)
+        filter?.setValue(0.8, forKey: "inputIntensity")
+        let outputImage = filter?.outputImage
+        
+        if self.cancelled {
+            return nil
+        }
+        
+        let outImage = context.createCGImage(outputImage!, fromRect: (outputImage?.extent)!)
+        let returnImage = UIImage(CGImage: outImage)
+        return returnImage
+    }
+}
 
 
 
